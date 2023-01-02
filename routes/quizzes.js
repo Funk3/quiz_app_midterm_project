f33676a0c5cdc9f6ca46a3fe1570fad34b46dce7
t-iffany <tiffanyxleong@gmail.com>
2023-01-02 23:35:43 +0000

    Add POST request to take data submitted into new quiz form

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -13,3 +13,36 @@ app.get('/account/:id/new', (req, res) => {
   }
   return res.render('quizzes_new', templateVars);
 });
+
+// Takes data submitted into new quiz form and creates a new quiz
+app.post('/quizzes', (req, res) => {
+  const newQuestion = req.body.question;
+  const newAnswerOne = req.body.answerOne;
+  const newAnswerTwo = req.body.answerTwo;
+  const newAnswerThree = req.body.answerThree;
+  const newAnswerFour = req.body.answerFour;
+
+  if(!usersDatabase[req.session.user_id]) {
+    return res.send('Please log in to create new quiz');
+  };
+
+  const newQuiz = {
+    newQuestion,
+    newAnswerOne,
+    newAnswerTwo,
+    newAnswerThree,
+    newAnswerFour
+  };
+
+  quizDatabase[newQuiz] = {
+    question: req.body.question,
+    answerOne: ,
+    answerTwo: ,
+    answerThree: ,
+    answerFour: ,
+    correctAnswerID: ,
+    userID: req.session.user_id
+  };
+
+  return res.redirect(`/quizzes/${newQuiz}`);
+});
